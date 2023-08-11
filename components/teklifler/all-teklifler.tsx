aeb510f8add9be933c446b921c51ac066fcfca49
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-08-11 17:09:24 +0300

    forms types

--- components/teklifler/all-teklifler.tsx
+++ components/teklifler/all-teklifler.tsx
@@ -7,28 +7,34 @@ import KaskoForm from '../teklif-forms/kasko-form'
 import KonutForm from '../teklif-forms/konut-form'
 import IsyeriForm from '../teklif-forms/isyeri-form'
 import DaskForm from '../teklif-forms/dask-form'
+import Image from 'next/image'
 
 
 const Buttons = [
     {
-        title: 'Trafik Sigortası Teklif Formu',
-        content: 'trafik'
+        title: 'Trafik',
+        content: 'trafik',
+        imageSrc: '/trafik.png'
     },
     {
-        title: 'Kasko Sigortası Teklif Formu',
-        content: 'kasko'
+        title: 'Kasko',
+        content: 'kasko',
+        imageSrc: '/kasko.png'
     },
     {
-        title: 'Konut Sigortası Teklif Formu',
-        content: 'konut'
+        title: 'Konut',
+        content: 'konut',
+        imageSrc: '/konut.png'
     },
     {
-        title: 'İşyeri Sigortası Teklif Formu',
-        content: 'isyeri'
+        title: 'İşyeri',
+        content: 'isyeri',
+        imageSrc: '/isyeri.png'
     },
     {
-        title: 'Dask Sigortası Teklif Formu',
-        content: 'dask'
+        title: 'Dask',
+        content: 'dask',
+        imageSrc: '/dask.png'
     }
 ]
 
@@ -38,16 +44,32 @@ const AllTeklifler = () => {
 
     return (
         <div className='flex flex-col gap-20'>
-            <div className='flex items-center gap-4 border-2 p-4 rounded-xl bg-main text-white flex-1 flex-wrap'>
+            <div className='flex items-center gap-4 md:space-x-12 rounded-xl flex-1 flex-wrap'>
                 {
                     Buttons.map((element) => {
                         return (
                             // <Button key={element.title} variant={element.content === content ? 'outline' : 'default'} onClick={() => setContent(element.content)}>
                             //     {element.title}
                             // </Button>
-                            <button key={element.title} onClick={() => setContent(element.content)}>
-                                {element.title}
-                            </button>
+                            <div
+                                key={element.title}
+                                className={`flex flex-col items-center justify-center gap-2 border border-border rounded-full py-2 px-4 md:py-4 md:px-8 cursor-pointer
+                                ${element.content === content && 'bg-main text-white'} 
+                            `}
+                                onClick={() => setContent(element.content)}
+                            >
+                                <div className='relative w-6 h-6 md:w-12 md:h-12'>
+                                    <Image
+                                        src={element.imageSrc}
+                                        fill
+                                        alt={element.title}
+                                    />
+                                </div>
+                                <span className='font-semibold text-xs md:text-base'>
+                                    {element.title}
+                                </span>
+                            </div>
+
                         )
                     })
                 }
