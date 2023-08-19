cd1fc84730faeebda032db8319be89f64f882370
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-08-19 12:34:47 +0300

    menu modal

--- components/header/teklifal-menu-modal.tsx
+++ components/header/teklifal-menu-modal.tsx
@@ -1,20 +1,26 @@
 'use client'
 import React from 'react'
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from '../ui/sheet'
-import { BiMenuAltRight } from 'react-icons/bi'
 import { FiMenu } from 'react-icons/fi'
 import { NavbarModal } from './navigation-modal'
+import Link from 'next/link'
+import Image from 'next/image'
 const TeklifAlMenuModal = () => {
     return (
         <Sheet>
             <SheetTrigger><FiMenu size={28} /></SheetTrigger>
             <SheetContent className='pt-12 ' side="top">
-                {/* <SheetHeader className=''>
-                    <SheetTitle>Are you sure absolutely sure ?</SheetTitle>
-                    <SheetDescription>
-                        This action cannot be undone . This will permanently delete your account and remoev . .. .
-                    </SheetDescription>
-                </SheetHeader> */}
+                <SheetHeader className='items-center pb-6'>
+                    <Link href='/' className='font-extrabold flex items-center space-x-3'>
+                        <Image
+                            src='/tick.png'
+                            width={40}
+                            height={40}
+                            alt='logo'
+                        />
+                        <span>maraşsigorta</span>
+                    </Link>
+                </SheetHeader>
                 <NavbarModal />
             </SheetContent>
         </Sheet>
